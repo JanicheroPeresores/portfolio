0cf0d0623cbf89aabaee68d35004835894b3d19d
JanicheroPeresores <156271328+JanicheroPeresores@users.noreply.github.com>
2025-12-05 19:49:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-const toggleBtn = document.getElementById("theme-toggle");
-
-toggleBtn.addEventListener("click", () => {
+// DARK MODE TOGGLE
+document.getElementById("theme-toggle").addEventListener("click", () => {
   document.body.classList.toggle("dark");
 });
 
+// IMAGE MODAL
+function openModal(imgSrc) {
+  const modal = document.getElementById("image-modal");
+  const modalImg = document.getElementById("modal-img");
+  modalImg.src = imgSrc;
+  modal.style.display = "flex";
+}
+
+function closeModal() {
+  document.getElementById("image-modal").style.display = "none";
+}
